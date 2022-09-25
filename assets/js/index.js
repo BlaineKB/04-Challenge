a90dec8e6144380c176e1157aaba5b258f1755b0
BKB <blainekb94@gmail.com>
2022-09-25 06:44:59 -0500

    updated html and added variables to grab elements by ID

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+// Declare variables to grab elements by ID's and start time/score/question array at beginning
+var time = 0;
+var score = 0;
+var question = -1;
+var startQuizBtn = document.querySelector("#startQuizBtn");
+var showHighScore = document.getElementById("highscore-name-input");
+var showQuestion = document.getElementById("quiz-box")
+
+console.log(showQuestion);
+// Array variable that contains all the questions, possible answer choices, and lastly the answer to the question
+
